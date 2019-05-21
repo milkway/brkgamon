3b0c1a86433f15a9bd463382c624305d12178714
milkway <leite@de.ufpe.br>
2019-05-21 10:49:17 +0000

    bench2

---
+++ benchmark2.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+
+benchmark <- read_rds("data/brkga.rds") %>% mutate(
+  Type = str_sub(Instancia, 6,8), 
+  SubType = str_sub(Instancia, 10,10), 
+  File = as.integer(str_remove(str_sub(Instancia, 12,13), "_")), 
+  n = as.integer(str_remove(str_extract(Instancia,pattern = "n\\d+"), pattern = "n")),
+  m = as.integer(str_remove(str_extract(Instancia,pattern = "m\\d+"), pattern = "m")),
+  Name = paste(SubType, File, sep = "-"),
+  Instancia = str_remove(str_remove(Instancia, pattern = "conv_"), pattern = ".txt"))
+
+
+brito <- read_rds("data/brito.rds")  %>% mutate(
+  Type = str_sub(Instancia, 1,3), 
+  SubType = str_sub(Instancia, 5,5), 
+  File = as.integer(str_remove(str_sub(Instancia, 7,8), "_")), 
+  n = as.integer(str_remove(str_extract(Instancia,pattern = "n\\d+"), pattern = "n")),
+  m = as.integer(str_remove(str_extract(Instancia,pattern = "m\\d+"), pattern = "m")),
+  Name = paste(SubType, File, sep = "-"))
+
+db <- benchmark  %>% 
+  left_join(brito, by = c("Instancia", "Type", "SubType", "File", "Name", "n", "m")) %>% 
+  mutate(LSEr = 100*(round(LS,2) - Target)/Target,
+         BKEr = 100*(round(BRKGA,2) - Target)/Target)
+
+
+
+db %>% ggplot() + 
+  geom_boxplot(aes(x = reorder(Method, LSEr, median), y = LSEr, fill = Method)) + 
+  coord_flip() + labs(x = "Method", y = "Error in %")
+
+
+db %>% ggplot() + 
+  geom_boxplot(aes(x = reorder(Method, N_gen, median), y = N_gen, fill = Method)) + 
+  coord_flip() + labs(x = "Method", y = "Number of Generations")
+
+
+db %>% ggplot() + 
+  geom_boxplot(aes(x = reorder(Method, N_bst, median), y = N_bst, fill = Method)) + 
+  coord_flip() + labs(x = "Method", y = "Best Generation")
+
+
+db %>% ggplot() + 
+  geom_boxplot(aes(x = reorder(Method, LSEr, median), y = LSEr, fill = Method)) + 
+  coord_flip() + labs(x = "Method", y = "Error in %") + facet_wrap(. ~ Name)
